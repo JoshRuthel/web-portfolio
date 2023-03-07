0dd5cf615bcc4b997998a5dd0a3032ae41123d84
JoshRuthel <joshuaruthless@gmail.com>
2023-03-07 16:17:25 +0200

    Grammer Changes in About

--- src/components/About.js
+++ src/components/About.js
@@ -15,23 +15,23 @@ function About() {
       <div class="bg-indigo-500 rounded-xl shadow-xl px-4 py-4 mt-8">
         <p class="text-black md:text-xl ml-4 md:ml-10 mt-1 font-bold shadow-md">
           I am 21 years old and currently in my Honours year of Information
-          Engineering at Wits{" "}
+          Engineering at Wits.
         </p>
         <p class="text-black md:text-xl ml-4 md:ml-10 mt-1 font-bold shadow-md">
           I started my software journey in 2020 and I am constantly trying
-          better my skillset
+          better my skillset.
         </p>
         <p class="text-black md:text-xl ml-4 md:ml-10 mt-1 font-bold shadow-md">
-          I enjoy all things technology including Artifical Intelligence, Cyber
-          Security and Software Development
+          I enjoy all things technology including: Artifical Intelligence, Cyber
+          Security and Software Development.
         </p>
         <p class="text-black md:text-xl ml-4 md:ml-10 mt-1 font-bold shadow-md">
           Outside of my studies I enjoy lifting weights, and spending quality
-          time with loved ones
+          time with loved ones.
         </p>
         <p class="text-black md:text-xl ml-4 md:ml-10 mt-1 font-bold shadow-md">
           I enjoy to be challenged and solving problems, which is why I feel
-          at home as a programmer
+          at home as a programmer.
         </p>
       </div>
       </AnimatedOnScroll>
